7cc5cd685e6458e7eee254fc47379dfd719fa7c6
Yash Singh Jha <87113569+YashIITM@users.noreply.github.com>
2023-06-11 13:49:29 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,9 @@
-# Leave it empty. This is just a special file that tells pip that your main module is in this folder. 
-# No need to add anything here. Feel free to delete this line when you make your own package.
+from .bandit import *
+from .linucb import *
+from .linucbvi import *
+from .mdp import *
+from .neuralucb import *
+from .neuralucbvi import *
+from .ucb import *
+from .ucbvi import *
+from .utils import *
